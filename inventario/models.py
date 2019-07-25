84f083da7fccc03cb4f69d8495a0f5e133ebe9e8
leonardo1497 <1497enrique@gmail.com>
2019-07-25 13:43:15 -0500

    cantidad json

--- inventario/models.py
+++ inventario/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class Inventario(models.Model):
     Modelo = models.CharField(max_length=200,null=False)
     Imagen = models.TextField(null=False)
     Precio = models.FloatField(null=False)
-    Cantidad = models.TextField()
+    Cantidad = JSONField()
